4adef992a9cfacf8a9a875dda05ad23ff002c7f7
Danilo Bellini <bellini.daan@gmail.com>
2023-03-28 14:12:19 -0300

    Exercicios pre aula

---
+++ fundamentos/secao-03-introducao-javascript/dia-01-javascript-primeirosPassos/exercicios-pre-aula/script.js
@@ -0,0 +1,69 @@
+/// Primeiro exercício ///
+
+// let myName = 'Danilo';
+// let birthCity = 'São Paulo';
+// let birthYear = 1995;
+
+// console.log(myName + ' nascido em', birthCity + ' no ano de',birthYear); 
+
+// /// Segundo exercício ///
+
+// let base = 5;
+// let height = 8;
+// let area = base * height;
+
+// console.log(area);
+
+// let perimeter = 5 + 5 + 8 + 8;
+
+// console.log(perimeter);
+
+/// Terceiro exercício ///
+
+// let nota = 70;
+
+// if (nota >= 80) {
+//     console.log('Parabéns, você faz parte do grupo de pessoas aprovadas!')
+// }
+// else if (nota < 80 && nota >= 60) {
+//     console.log('Você está na nossa lista de espera.')
+// }
+// else if (nota < 60) {
+//     console.log('Infelizmente você reprovou.')
+// }
+
+/// Quarto exercício ///
+
+// let currentHour = 14;
+// let message;
+
+// if (currentHour >= 22) {
+//     message = 'Não deveríamos comer nada, é hora de dormir'
+// }
+// else if (currentHour >= 18 && currentHour < 22) {
+//     message = 'Rango da noite, vamos jantar :D'
+// }
+// else if (currentHour >= 14 && currentHour < 18) {
+//     message = 'Vamos fazer um bolo pro café da tarde?'
+// }
+// else if (currentHour >= 11 && currentHour < 14) {
+//     message = 'Hora do almoço!!!'
+// }
+// else if (currentHour = 4 && 11 ) {
+//     message = 'Hmmm, cheiro de café recém-passado'
+// }
+
+// console.log(message);
+
+/// Quinto exercício ///
+
+// let weekDay = 'sexta-feira';
+
+// if (weekDay === 'sabado' || weekDay === 'domingo') {
+//     console.log('Descanso merecido!')
+// }
+// else {
+//     console.log('Dia de aula na Trybe!')
+// }
+
+/// Sexto exercício ///
